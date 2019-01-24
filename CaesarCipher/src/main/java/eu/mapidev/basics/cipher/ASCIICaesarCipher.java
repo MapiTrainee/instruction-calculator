29163cc905b4d13f38dafcc906993b022845c069
Mark <mpietryga93@gmail.com>
2019-01-24 15:02:05 +0100

    Implement encrypt method with some unit tests

--- CaesarCipher/src/main/java/eu/mapidev/basics/cipher/ASCIICaesarCipher.java
+++ CaesarCipher/src/main/java/eu/mapidev/basics/cipher/ASCIICaesarCipher.java
@@ -1,10 +1,29 @@
 package eu.mapidev.basics.cipher;
 
+import javax.xml.stream.events.Characters;
+
 public class ASCIICaesarCipher implements CaesarCipher {
 
     @Override
     public String encrypt(String plaintext, int key) {
-	throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	key = key % 26;
+	char[] input = plaintext.toCharArray();
+	for (int i = 0; i < input.length; i++) {
+	    boolean isLowerCases = false;
+	    if (Character.isLowerCase(input[i])) {
+		isLowerCases = true;
+		input[i] = Character.toUpperCase(input[i]);
+	    }
+	    char encoded;
+	    if ((input[i] + key) > 90) {
+		int shift = input[i] + key - 90;
+		encoded = (char) (64 + shift);
+	    } else {
+		encoded = (char) (input[i] + key);
+	    }
+	    input[i] = isLowerCases ? Character.toLowerCase(encoded) : encoded;
+	}
+	return new String(input);
     }
 
     @Override
--- CaesarCipher/src/test/java/eu/mapidev/basics/cipher/ASCIICaesarCipherTest.java
+++ CaesarCipher/src/test/java/eu/mapidev/basics/cipher/ASCIICaesarCipherTest.java
@@ -1,5 +1,50 @@
 package eu.mapidev.basics.cipher;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.collection.IsArrayContainingInOrder.arrayContaining;
+import static org.junit.Assert.assertThat;
+import org.junit.Before;
+import org.junit.Test;
+
 public class ASCIICaesarCipherTest {
+
+    private CaesarCipher caesarCipher;
+
+    @Before
+    public void setUp() {
+	caesarCipher = new ASCIICaesarCipher();
+    }
+
+    @Test
+    public void shouldNotEncryptWhenKeyIs0() {
+	String plaintext = "ABCabc";
+	int key = 0;
+	String secret = caesarCipher.encrypt(plaintext, key);
+	assertThat(secret, is(plaintext));
+    }
+
+    @Test
+    public void shouldEncryptABCabcToBCDbcdWhenKeyIs1() {
+	String plaintext = "ABCabc";
+	int key = 1;
+	String secret = caesarCipher.encrypt(plaintext, key);
+	assertThat(secret, is("BCDbcd"));
+    }
+
+    @Test
+    public void shouldEncryptABCabcToZABzabWhenKeyIs25() {
+	String plaintext = "ABCabc";
+	int key = 25;
+	String secret = caesarCipher.encrypt(plaintext, key);
+	assertThat(secret, is("ZABzab"));
+    }
+
+    @Test
+    public void shouldEncryptABCabcToCDEcdeWhenKeyIs28() {
+	String plaintext = "ABCabc";
+	int key = 28;
+	String secret = caesarCipher.encrypt(plaintext, key);
+	assertThat(secret, is("CDEcde"));
+    }
 
 }
