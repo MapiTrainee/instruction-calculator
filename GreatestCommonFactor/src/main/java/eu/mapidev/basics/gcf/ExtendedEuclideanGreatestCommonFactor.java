1faf7aff369a240b068df09455ee747ac8afb6f5
Mark <mpietryga93@gmail.com>
2019-02-01 13:09:54 +0100

    Add extended euclidean algorithm based on recursion with a simple test

--- GreatestCommonFactor/src/main/java/eu/mapidev/basics/gcf/ExtendedEuclideanGreatestCommonFactor.java
+++ GreatestCommonFactor/src/main/java/eu/mapidev/basics/gcf/ExtendedEuclideanGreatestCommonFactor.java
@@ -4,8 +4,25 @@ import java.util.function.BiFunction;
 
 public class ExtendedEuclideanGreatestCommonFactor {
 
-    public static BiFunction<Integer, Integer, Integer[]> findRecursively() {
-	throw new UnsupportedOperationException();
+    public static BiFunction<Integer, Integer, Integer[]> recursiveFinder() {
+	return new BiFunction<Integer, Integer, Integer[]>() {
+	    private Integer[] p = {0, 0};
+
+	    @Override
+	    public Integer[] apply(Integer a, Integer b) {
+		if (b == 0) {
+		    return new Integer[]{1, 0};
+		} else {
+		    p = apply(b, a % b);
+		    Integer q = a / b;
+		    return new Integer[]{p[1], p[0] - q * p[1]};
+		}
+	    }
+	};
+    }
+
+    public static BiFunction<Integer, Integer, Integer[]> iterativeFinder() {
+	throw new UnsupportedOperationException("Not implemented yet!");
     }
 
 }
--- GreatestCommonFactor/src/test/java/eu/mapidev/basics/gcf/ExtendedEuclideanGreatestCommonFactorTest.java
+++ GreatestCommonFactor/src/test/java/eu/mapidev/basics/gcf/ExtendedEuclideanGreatestCommonFactorTest.java
@@ -1,6 +1,19 @@
 package eu.mapidev.basics.gcf;
 
+import java.util.function.BiFunction;
+import static org.hamcrest.Matchers.arrayContaining;
+import static org.junit.Assert.assertThat;
+import org.junit.Test;
+
 public class ExtendedEuclideanGreatestCommonFactorTest {
-    
-    
+
+    @Test
+    public void recursiveFinderShouldReturnCorrectCoefficientsForThisExample() {
+	Integer a = 188;
+	Integer b = 12;
+	Integer[] expectedCoefficents = {-1, 16};
+
+	BiFunction<Integer, Integer, Integer[]> finder = ExtendedEuclideanGreatestCommonFactor.recursiveFinder();
+	assertThat(finder.apply(a, b), arrayContaining(expectedCoefficents));
+    }
 }
