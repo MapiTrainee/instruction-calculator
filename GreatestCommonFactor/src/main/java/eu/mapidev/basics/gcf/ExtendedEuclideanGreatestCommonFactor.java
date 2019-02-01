4c366255c9f0e6daceb4f59c8e142e0f3e51e6e8
Mark <mpietryga93@gmail.com>
2019-02-01 14:06:08 +0100

    Improve finders to validate inputs ...
    This commit includes:
    - a parameterized set of tests,
    - updated implementation of finder methods.

--- GreatestCommonFactor/src/main/java/eu/mapidev/basics/gcf/ExtendedEuclideanGreatestCommonFactor.java
+++ GreatestCommonFactor/src/main/java/eu/mapidev/basics/gcf/ExtendedEuclideanGreatestCommonFactor.java
@@ -3,31 +3,43 @@ package eu.mapidev.basics.gcf;
 import java.util.function.BiFunction;
 
 public class ExtendedEuclideanGreatestCommonFactor {
-
+    
     public static BiFunction<Integer, Integer, Integer[]> recursiveFinder() {
 	return new BiFunction<Integer, Integer, Integer[]>() {
 	    private Integer[] p;
 	    private Integer q;
-
+	    
 	    @Override
 	    public Integer[] apply(Integer a, Integer b) {
+		validateInputValues(a, b);
+		return find(a, b);
+	    }
+	    
+	    public Integer[] find(Integer a, Integer b) {
 		if (b == 0) {
 		    return new Integer[]{1, 0};
 		} else {
-		    p = apply(b, a % b);
+		    p = find(b, a % b);
 		    q = a / b;
 		    return new Integer[]{p[1], p[0] - q * p[1]};
 		}
 	    }
 	};
     }
-
+    
+    private static void validateInputValues(Integer a, Integer b) throws IllegalArgumentException {
+	if (a == null || b == null || a <= 0 || b <= 0) {
+	    throw new IllegalArgumentException("Input numbers must be positive and not null!");
+	}
+    }
+    
     public static BiFunction<Integer, Integer, Integer[]> iterativeFinder() {
 	return (a, b) -> {
+	    validateInputValues(a, b);
 	    int prevX = 1, prevY = 0;
 	    int x = 0, y = 1;
 	    int q, r, newX, newY;
-
+	    
 	    while (b != 0) {
 		q = a / b;
 		r = a % b;
@@ -43,5 +55,5 @@ public class ExtendedEuclideanGreatestCommonFactor {
 	    return new Integer[]{prevX, prevY};
 	};
     }
-
+    
 }
--- GreatestCommonFactor/src/test/java/eu/mapidev/basics/gcf/ExtendedEuclideanGreatestCommonFactorTest.java
+++ GreatestCommonFactor/src/test/java/eu/mapidev/basics/gcf/ExtendedEuclideanGreatestCommonFactorTest.java
@@ -1,29 +1,64 @@
 package eu.mapidev.basics.gcf;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.function.BiFunction;
 import static org.hamcrest.Matchers.arrayContaining;
 import static org.junit.Assert.assertThat;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+@RunWith(Parameterized.class)
 public class ExtendedEuclideanGreatestCommonFactorTest {
 
-    @Test
-    public void recursiveFinderShouldReturnCorrectCoefficientsForThisExample() {
-	Integer a = 188;
-	Integer b = 12;
-	Integer[] expectedCoefficents = {-1, 16};
+    @Parameterized.Parameter(0)
+    public Integer inputA;
+    @Parameterized.Parameter(1)
+    public Integer inputB;
+    @Parameterized.Parameter(2)
+    public Integer[] expectedCoefficients;
+    @Parameterized.Parameter(3)
+    public Class<? extends Exception> expectedException;
 
-	BiFunction<Integer, Integer, Integer[]> finder = ExtendedEuclideanGreatestCommonFactor.recursiveFinder();
-	assertThat(finder.apply(a, b), arrayContaining(expectedCoefficents));
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Parameterized.Parameters
+    public static Collection data() {
+	return Arrays.asList(new Object[][]{
+	    {null, 1, null, IllegalArgumentException.class},
+	    {1, null, null, IllegalArgumentException.class},
+	    {-1, 1, null, IllegalArgumentException.class},
+	    {1, -1, null, IllegalArgumentException.class},
+	    {1, 0, null, IllegalArgumentException.class},
+	    {0, 1, null, IllegalArgumentException.class},
+	    {1, 1, new Integer[]{0, 1}, null},
+	    {188, 12, new Integer[]{-1, 16}, null},
+	    {12, 188, new Integer[]{16, -1}, null},
+	    {30, 20, new Integer[]{1, -1}, null},
+	    {20, 30, new Integer[]{-1, 1}, null}
+	});
+
     }
 
     @Test
-    public void iterativeFinderShouldReturnCorrectCoefficientsForThisExample() {
-	Integer a = 188;
-	Integer b = 12;
-	Integer[] expectedCoefficents = {-1, 16};
+    public void recursiveFinderShouldReturnExpectedCoefficientsAccordingToData() {
+	if (expectedException != null) {
+	    thrown.expect(expectedException);
+	}
+	BiFunction<Integer, Integer, Integer[]> finder = ExtendedEuclideanGreatestCommonFactor.recursiveFinder();
+	assertThat(finder.apply(inputA, inputB), arrayContaining(expectedCoefficients));
+    }
 
+    @Test
+    public void iterativeFinderShouldReturnExpectedCoefficientsAccordingToData() {
+	if (expectedException != null) {
+	    thrown.expect(expectedException);
+	}
 	BiFunction<Integer, Integer, Integer[]> finder = ExtendedEuclideanGreatestCommonFactor.iterativeFinder();
-	assertThat(finder.apply(a, b), arrayContaining(expectedCoefficents));
+	assertThat(finder.apply(inputA, inputB), arrayContaining(expectedCoefficients));
     }
 }
