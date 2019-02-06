892eb9918aea2f69cbcc2050940bfb5e8064a2dc
Mark <mpietryga93@gmail.com>
2019-02-06 12:20:33 +0100

    Implement a method, which finds the message with the largest number of different characters

--- Wega/src/main/java/eu/mapidev/basics/task/wega/SignalFinder.java
+++ Wega/src/main/java/eu/mapidev/basics/task/wega/SignalFinder.java
@@ -1,10 +1,14 @@
 package eu.mapidev.basics.task.wega;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class SignalFinder {
 
     private final List<String> signals;
+    private boolean[] visitedCharacters = new boolean[26];
+    private int theLargestNumberOfDifferentCharacters;
+    private String theMessageWithTheLargestNumberOfDifferentCharacters;
 
     public SignalFinder(List<String> signals) {
 	this.signals = signals;
@@ -18,5 +22,38 @@ public class SignalFinder {
 	    sb.append(tenthCharacter);
 	}
 	return sb.toString();
+    }
+
+    public String findMessageWithTheLargetstNumberOfDifferentCharacters() {
+	theLargestNumberOfDifferentCharacters = Integer.MIN_VALUE;
+	theMessageWithTheLargestNumberOfDifferentCharacters = null;
+
+	for (String signal : signals) {
+	    Arrays.fill(visitedCharacters, false);
+	    visitAllCharactersOfString(signal);
+	    int visitedCharactersCounter = getVisitedCharactersCounter();
+	    if (visitedCharactersCounter > theLargestNumberOfDifferentCharacters) {
+		theMessageWithTheLargestNumberOfDifferentCharacters = signal;
+		theLargestNumberOfDifferentCharacters = visitedCharactersCounter;
+	    }
+	}
+	return theMessageWithTheLargestNumberOfDifferentCharacters;
+    }
+
+    private int getVisitedCharactersCounter() {
+	int counter = 0;
+	for (int i = 0; i < visitedCharacters.length; i++) {
+	    if (visitedCharacters[i]) {
+		counter++;
+	    }
+	}
+	return counter;
+    }
+
+    private void visitAllCharactersOfString(String input) {
+	char[] characters = input.toCharArray();
+	for (int i = 0; i < characters.length; i++) {
+	    visitedCharacters[characters[i] - 65] = true;
+	}
     }
 }
--- Wega/src/test/java/eu/mapidev/basics/task/wega/SignalFinderTest.java
+++ Wega/src/test/java/eu/mapidev/basics/task/wega/SignalFinderTest.java
@@ -2,6 +2,7 @@ package eu.mapidev.basics.task.wega;
 
 import java.io.File;
 import java.util.List;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import org.junit.Test;
@@ -10,19 +11,27 @@ public class SignalFinderTest {
 
     public SimpleLineReader simpleLineReader;
     public List<String> lines;
+    private final SignalFinder finder;
 
     public SignalFinderTest() {
 	ClassLoader cl = ClassLoader.getSystemClassLoader();
 	simpleLineReader = new SimpleLineReader(new File(
 		cl.getResource("examples.txt").getPath()));
 	lines = simpleLineReader.readAll();
+	finder = new SignalFinder(lines);
     }
 
     @Test
     public void shouldAllowToFindSecretMessageFromWega() {
-	SignalFinder sf = new SignalFinder(lines);
-	String actualMessage = sf.readSecretMessage();
+	String actualMessage = finder.readSecretMessage();
 	assertThat(actualMessage, is("NIECHCIMATURAPROSTABEDZIE"));
+    }
+
+    @Test
+    public void shouldFindTheMessageWithTheLargestNumberOfDifferentCharacters() {
+	String actualMessage = finder.findMessageWithTheLargetstNumberOfDifferentCharacters();
+	assertThat(actualMessage.length(), equalTo("AKLMNOPRSTWZA".length()));
+	assertThat(actualMessage, is("AKLMNOPRSTWZA"));
     }
 
 }
